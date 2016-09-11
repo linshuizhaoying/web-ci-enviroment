fedfb301b25f6eefb62badca5fa16eba93a6c8b8
linshuizhaoying <4799109@qq.com>
2016-09-12 02:42:08 +0800

    ci test4

--- test/unit/specs/test.spec.js
+++ test/unit/specs/test.spec.js
@@ -4,6 +4,6 @@ import Hello from 'src/components/Hello'
 describe('Hello.vue', () => {
   it('should contain', () => {
     expect(Hello.data).to.be.a('function')
-    expect(Hello.data().msg).to.eql('ddd!')
+    expect(Hello.data().msg).to.eql('22ddd!')
   })
 })
